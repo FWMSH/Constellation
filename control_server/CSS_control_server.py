1726636c267b86fcb76d4d4aac01331f58ffaf3c
Morgan Rehnberg <forceflow1049@gmail.com>
2021-02-03 13:32:07 -0600

    More basic framework

--- control_server/CSS_control_server.py
+++ control_server/CSS_control_server.py
@@ -5,7 +5,65 @@ from datetime import datetime
 ADDR = "" # Accept connections from all interfaces
 PORT = 8082
 
+class ExhibitComponent:
+
+    # Holds basic data about a component in the exhibit
+
+    def __init__(self, id):
+
+        self.id = id
+        self.lastContactDateTime = datetime.now()
+        self.lastInteractionDateTime = datetime(2020, 1, 1)
+
+    def secondsSinceLastContact(self):
+
+        diff = datetime.now() - self.lastContactDateTime
+        return(diff.total_seconds())
+
+    def secondsSinceLastInteraction(self):
+
+        diff = datetime.now() - self.lastInteractionDateTime
+        return(diff.total_seconds())
+
+    def currentStatus(self):
+
+        # Return the current status of the component
+        # [OFFLINE, ONLINE, ACTIVE, WAITING]
+
+        status = 'OFFLINE'
+
+        if self.secondsSinceLastContact() < 30:
+            if self.secondsSinceLastInteraction() < 10:
+                status = "ACTIVE"
+            else:
+                status = "ONLINE"
+        elif self.secondsSinceLastContact() < 300:
+            status = "WAITING"
+
+        return(status)
+
+
 class RequestHandler(SimpleHTTPRequestHandler):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.componentList = []
+
+    def getExhibitComponent(self, id):
+
+        # Return a component with the given id, or None if no such
+        # component exists
+
+        component = next((x for x in self.componentList if x.id == id), None)
+
+        return(component)
+
+    def addExhibitComponent(self, id):
+
+        component = ExhibitComponent(id)
+        self.componentList.append(component)
 
     def log_request(code='-', size='-'):
 
@@ -15,11 +73,14 @@ class RequestHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
 
-        pass
+        # Receive a GET request and respond with a console webpage
+
+        print('GET received... ')
 
     def do_POST(self):
 
-        # Receives pings from client devices
+        # Receives pings from client devices and respond with any updated
+        # information
 
         print('POST received... ')
         self.send_response(200, "OK")
