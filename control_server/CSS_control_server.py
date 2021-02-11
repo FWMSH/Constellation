d16d3115ea1b01dbcc82de2aab77fcd93c19e2b0
Morgan Rehnberg <forceflow1049@gmail.com>
2021-02-11 13:24:35 -0600

    Added tabs and the schedule functionality

--- control_server/CSS_control_server.py
+++ control_server/CSS_control_server.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import time
-from datetime import datetime
+import datetime
+import dateutil.parser
 import configparser
 import json
 
@@ -17,8 +18,8 @@ class ExhibitComponent:
         self.type = type
         self.ip = "" # IP address of client
 
-        self.lastContactDateTime = datetime.now()
-        self.lastInteractionDateTime = datetime(2020, 1, 1)
+        self.lastContactDateTime = datetime.datetime.now()
+        self.lastInteractionDateTime = datetime.datetime(2020, 1, 1)
 
         self.config = {}
         self.config["commands"] = []
@@ -26,12 +27,12 @@ class ExhibitComponent:
 
     def secondsSinceLastContact(self):
 
-        diff = datetime.now() - self.lastContactDateTime
+        diff = datetime.datetime.now() - self.lastContactDateTime
         return(diff.total_seconds())
 
     def secondsSinceLastInteraction(self):
 
-        diff = datetime.now() - self.lastInteractionDateTime
+        diff = datetime.datetime.now() - self.lastInteractionDateTime
         return(diff.total_seconds())
 
     def updateLastContactDateTime(self):
@@ -39,14 +40,14 @@ class ExhibitComponent:
         # We've received a new ping from this component, so update its
         # lastContactDateTime
 
-        self.lastContactDateTime = datetime.now()
+        self.lastContactDateTime = datetime.datetime.now()
 
     def updateLastInteractionDateTime(self):
 
         # We've received a new interaction ping, so update its
         # lastInteractionDateTime
 
-        self.lastInteractionDateTime = datetime.now()
+        self.lastInteractionDateTime = datetime.datetime.now()
 
     def currentStatus(self):
 
@@ -78,6 +79,7 @@ class ExhibitComponent:
 
     def queueCommand(self, command):
 
+        print(f"{self.id}: command queued: {command}")
         self.config["commands"].append(command)
 
 class RequestHandler(SimpleHTTPRequestHandler):
@@ -112,9 +114,34 @@ class RequestHandler(SimpleHTTPRequestHandler):
         dict["currentExhibit"] = currentExhibit
         componentDictList.append(dict)
 
+        # Also include an object containing the current schedule
+        dict = {}
+        dict["class"] = "schedule"
+        for key in schedule_dict:
+            if key != "Next event":
+                dict[key] = schedule_dict[key].strftime("%I:%M %p")
+            else:
+                nextTime, nextAction = schedule_dict[key]
+                dict["Next time"] = nextTime.strftime("%A, %I:%M %p")
+                dict["Next action"] = nextAction
+        componentDictList.append(dict)
+
+
         json_string = json.dumps(componentDictList)
 
         self.wfile.write(bytes(json_string, encoding="UTF-8"))
+
+    def checkEventSchedule(self):
+
+        # Read the "Next event" tuple in schedule_dict and take action if necessary
+
+        global schedule_dict
+
+        nextEventDateTime, nextAction = schedule_dict["Next event"]
+
+        if datetime.datetime.now() > nextEventDateTime:
+            commandAllExhibitComponents(nextAction)
+            queueNextOnOffEvent()
 
     def log_request(code='-', size='-'):
 
@@ -211,12 +238,54 @@ class RequestHandler(SimpleHTTPRequestHandler):
                 print("Error: exhibitComponent ping received without id or type field")
                 return() # No id or type, so bail out
 
+            self.checkEventSchedule()
             updateExhibitComponentStatus(data, self.address_string())
             self.sendCurrentConfiguration(id)
         else:
             print(f"Error: ping with unknown class '{pingClass}' received")
             return() # Bail out
 
+def readSchedule(schedule):
+
+    # Take the schedule as a configparser section and parse it to build
+    # the dictionary used to turn the components on/off
+
+    global schedule_dict
+
+    for key in schedule:
+        # Convert the time, e.g. "9 AM" into a datetime time
+        schedule_dict[key] = dateutil.parser.parse(schedule[key]).time()
+
+def queueNextOnOffEvent():
+
+    # Function to consult schedule_dict and set the next datetime that we should
+    # send an on or off command
+
+    global schedule_dict
+
+    now = datetime.datetime.now() # Right now
+    eventDate = datetime.datetime.now().date() # When the event is (start now and we will advance it)
+    nextEventDateTime = None
+
+    while nextEventDateTime is None:
+        day_str = eventDate.strftime('%A').lower() # e.g., "monday"
+
+        if day_str+"_on" in schedule_dict:
+            on_time = datetime.datetime.combine(eventDate, schedule_dict[day_str+"_on"])
+            if now < on_time: # We are before today's on time
+                nextEventDateTime = on_time
+                nextAction = "wakeDisplay"
+            elif day_str+"_off" in schedule_dict:
+                off_time = datetime.datetime.combine(eventDate, schedule_dict[day_str+"_off"])
+                if now < off_time: # We are before today's off time
+                    nextEventDateTime = off_time
+                    nextAction = "sleepDisplay"
+
+        # If we are neither before the on time or the off time, go to tomorrow and loop again
+        eventDate += datetime.timedelta(days=1)
+
+    schedule_dict["Next event"] = (nextEventDateTime, nextAction)
+    print(f"New event queued: {nextAction}, {nextEventDateTime}")
 
 def loadCurrentExhibitConfiguration():
 
@@ -235,6 +304,9 @@ def loadCurrentExhibitConfiguration():
     currentExhibit = current["currentConfigFile"]
     serverPort = current.getint("server_port", 8080)
     ip_address = current.get("server_ip_address", "localhost")
+
+    schedule = config["SCHEDULE"]
+    readSchedule(schedule)
 
     # Then, load the configuration for that exhibit
     currentExhibitConfiguration = configparser.ConfigParser()
@@ -256,6 +328,13 @@ def addExhibitComponent(id, type):
 
     return(component)
 
+def commandAllExhibitComponents(cmd):
+
+    # Queue a command for every exhibit component
+
+    for component in componentList:
+        component.queueCommand(cmd)
+
 def updateExhibitComponentStatus(data, ip):
 
     id = data["id"]
@@ -276,8 +355,10 @@ ip_address = "localhost" # Default; should be set in exhibit INI file
 componentList = []
 currentExhibit = None # The INI file defining the current exhibit "name.exhibit"
 currentExhibitConfiguration = None # the configParser object holding the current config
+schedule_dict = {} # Will hold a list of on/off times for every day of the week
 
 loadCurrentExhibitConfiguration()
+queueNextOnOffEvent()
 
 httpd = HTTPServer((ADDR, serverPort), RequestHandler)
 httpd.serve_forever()
