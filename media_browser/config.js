83320c6081d2c65e9e6468c91d8a66dbd5eff333
Morgan Rehnberg <forceflow1049@gmail.com>
2021-11-01 14:09:54 -0500

    Starting to generalize

--- media_browser/config.js
+++ media_browser/config.js
@@ -1,1 +1,8 @@
 var helperAddress = "http://localhost:8000";
+
+var mediaKey = "Media";
+var thumbnailKey = null;
+var searchKeys = ["Last Name", "First"];
+var titleKey = "Entry Title";
+var captionKey = null;
+var creditKey = "credit";
